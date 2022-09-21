bcba440568bf78e8964e813f841c73868fc5950b
nikinsaw <nikinsaw@gmail.com>
2022-09-21 11:12:47 +0530

    removed unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useCallback } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
