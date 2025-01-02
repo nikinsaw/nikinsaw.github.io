b9427f2e947da1a5efbe4e626adaf738a279824d
nikinsaw <nikinsaw@gmail.com>
2025-01-02 19:13:41 +0530

    add logic to handle years of experience

---
+++ src/helpers/constants.js
@@ -0,0 +1,6 @@
+export const MONTHS_IN_A_YEAR = 12
+export const DAYS_IN_A_YEAR = 365
+export const HOURS_IN_DAY = 24
+export const MINUTES_IN_AN_HOUR = 60
+export const SECONDS_IN_A_MINUTE = 60
+export const MILLISECONDS_IN_SECOND = 1000
